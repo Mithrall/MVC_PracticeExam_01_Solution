082c037fe55a868d925aabc5fe0a5d7f0c7d1e8a
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2018-01-02 16:25:47 +0100

    Add project files.

---
+++ MvcWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcWebApplication.Models;
+
+namespace MvcWebApplication.Controllers {
+    public class HomeController : Controller {
+        List<BlogPost> tempList;
+        BlogPost post = new BlogPost {
+            ID = 0,
+            Title = "Test",
+            Content = "lalalal",
+            CreateDate = DateTime.Now,
+            Author = "Bob",
+        };
+
+
+        public IActionResult Index() {
+
+            if(BlogPostRepository.blogPostList.Count() <= 0) {
+
+
+
+                BlogPostRepository.blogPostList.Add(post);
+                tempList = new List<BlogPost>(BlogPostRepository.blogPostList);
+            }
+            return View(tempList);
+        }
+        [HttpPost]
+        public IActionResult Index(string Author) {
+            tempList = BlogPostRepository.blogPostList.FindAll(x => x.Author == Author);
+            
+            //Session["Author"] = Author;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcWebApplication/Models/BlogPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWebApplication.Models {
+    public class BlogPost {
+        public int ID;
+        public string Title;
+        public string Content;
+        public DateTime CreateDate;
+        public string Author;
+    }
+}
