f59c5398ac5887a115fa33ce2a0ceed968990081
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2018-01-02 19:50:44 +0100

    saves Author name to session

--- MvcWebApplication/Controllers/HomeController.cs
+++ MvcWebApplication/Controllers/HomeController.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MvcWebApplication.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Http;
 
 namespace MvcWebApplication.Controllers {
     public class HomeController : Controller {
+
         List<BlogPost> tempList;
         BlogPost post = new BlogPost {
             ID = 0,
@@ -17,12 +20,17 @@ namespace MvcWebApplication.Controllers {
             Author = "Bob",
         };
 
-
         public IActionResult Index() {
 
+            try {
+                var Author = HttpContext.Session.GetString("Author");
+                tempList = BlogPostRepository.blogPostList.FindAll(x => x.Author == Author);
+
+            } catch(Exception) {
+                throw;
+            }
+
             if(BlogPostRepository.blogPostList.Count() <= 0) {
-
-
 
                 BlogPostRepository.blogPostList.Add(post);
                 tempList = new List<BlogPost>(BlogPostRepository.blogPostList);
@@ -32,8 +40,8 @@ namespace MvcWebApplication.Controllers {
         [HttpPost]
         public IActionResult Index(string Author) {
             tempList = BlogPostRepository.blogPostList.FindAll(x => x.Author == Author);
-            
-            //Session["Author"] = Author;
+
+            HttpContext.Session.SetString("Author", Author);
 
             return RedirectToAction("Index");
         }
